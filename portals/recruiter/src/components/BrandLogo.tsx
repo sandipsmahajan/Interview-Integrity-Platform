b8d9ac0905f0bcdd5baf2c26e9516563262d1ae1
Sandip Mahajan <42683645+sandipsmahajan@users.noreply.github.com>
2026-08-04 20:37:52 +0530

    logos replaced

--- portals/recruiter/src/components/BrandLogo.tsx
+++ portals/recruiter/src/components/BrandLogo.tsx
@@ -1,47 +1,46 @@
 import Box from '@mui/material/Box';
 
 interface BrandMarkProps {
-  size?: number;
+    size?: number;
 }
 
-export function BrandMark({ size = 34 }: BrandMarkProps) {
-  return (
-    <Box
-      component="img"
-      src="/logos/vertical_logo.jpeg"
-      alt="Integrity Pro"
-      loading="lazy"
-      sx={{
-        width: size,
-        height: size,
-        borderRadius: 2,
-        objectFit: 'cover',
-        bgcolor: '#fff',
-        flexShrink: 0
-      }}
-    />
-  );
+export function BrandMark({size = 34}: BrandMarkProps) {
+    return (
+        <Box
+            component="img"
+            src="/logos/vertical_logo.png"
+            alt="Integrity Pro"
+            loading="lazy"
+            sx={{
+                width: size,
+                height: size,
+                borderRadius: 2,
+                objectFit: 'cover',
+                bgcolor: '#fff',
+                flexShrink: 0
+            }}
+        />
+    );
 }
 
 interface BrandBannerProps {
-  maxWidth?: number | string;
+    maxWidth?: number | string;
 }
 
-export function BrandBanner({ maxWidth = 200 }: BrandBannerProps) {
-  return (
-    <Box
-      component="img"
-      src="/logos/horizontal_logo.jpeg"
-      alt="Integrity Pro"
-      loading="lazy"
-      sx={{
-        width: '100%',
-        maxWidth,
-        height: 'auto',
-        borderRadius: 2,
-        objectFit: 'contain',
-        bgcolor: '#fff'
-      }}
-    />
-  );
+export function BrandBanner({maxWidth = 200}: BrandBannerProps) {
+    return (
+        <Box
+            component="img"
+            src="/logos/horizontal_logo.png"
+            alt="Integrity Pro"
+            loading="lazy"
+            sx={{
+                width: '200%',
+                maxWidth,
+                height: 'auto',
+                borderRadius: 0,
+                objectFit: 'contain'
+            }}
+        />
+    );
 }
